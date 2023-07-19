bab9d4d6c79f918d6260fa630809bebb444f1896
daryaagafonova <ADashyta2013@yandex.ru>
2023-07-19 21:16:00 +0300

    the tests in the test_get and test_my_slice functions from the file have been fixed test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,21 @@
 import unittest
-from utils import arrs
+from utils.arrs import get, my_slice
 
 
-class TestArrs(unittest.TestCase):
+class TestGet(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([], 0, "test"), "test")
+
+
+class TestMy_Slice(unittest.TestCase):
+
+    def test_my_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
