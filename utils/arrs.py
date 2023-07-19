be03dc1bb9541254f410b340e320c748e1971c80
daryaagafonova <ADashyta2013@yandex.ru>
2023-07-19 21:17:16 +0300

    the get from file function has been fixed arrs.py for conducting tests

--- utils/arrs.py
+++ utils/arrs.py
@@ -13,8 +13,10 @@ def get(array, index, default=None):
     """
     if index < 0:
         return default
-
-    return array[index]
+    elif index in array:
+        return array[index]
+    else:
+        return default
 
 
 def my_slice(coll, start=0, end=None):
