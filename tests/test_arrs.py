c58e301a53733b6eff3bf60b952626d441f1c1d0
daryaagafonova <ADashyta2013@yandex.ru>
2023-07-19 22:35:05 +0300

    tests were added to the test_get and test_my_slice function from the file test_arrs.py for 100 percent code coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,6 +8,7 @@ class TestGet(unittest.TestCase):
 
         self.assertEqual(get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(get([], 0, "test"), "test")
+        self.assertEqual(get([1, 2, 3], -1, "test"), "test")
 
 
 class TestMy_Slice(unittest.TestCase):
@@ -15,6 +16,9 @@ class TestMy_Slice(unittest.TestCase):
     def test_my_slice(self):
         self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([]), [])
+        self.assertEqual(my_slice([1, 2, 3], -1, 3), [3])
+        self.assertEqual(my_slice([1, 2], -3), [1, 2])
 
 
 if __name__ == '__main__':
